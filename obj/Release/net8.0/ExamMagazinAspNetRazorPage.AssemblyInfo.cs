57e0b4b71ce28512b7b017409946d9deeb1b194f
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2024-10-23 16:18:36 +0700

    реализовано добавление в корзину

--- obj/Release/net8.0/ExamMagazinAspNetRazorPage.AssemblyInfo.cs
+++ obj/Release/net8.0/ExamMagazinAspNetRazorPage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamMagazinAspNetRazorPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab751d479c7c3d771ba477633b2bbbcd1743b601")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec1ccb8bd1606a84400d8963d9da3015d4d3d3a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamMagazinAspNetRazorPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamMagazinAspNetRazorPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
